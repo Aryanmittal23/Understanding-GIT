688a8c3c9a7412ef7cf6bc95a8ca01c85c8242f7
Aryan Mittal <aryanmittal@Aryans-MacBook-Air.local>
2023-09-07 03:36:40 +0530

    initial commit

---
+++ javascrpit/script.js
@@ -0,0 +1,19 @@
+//let first = 12;
+//let second = 23;
+//number
+//let x = 15;
+//let age = 21;
+//let name="Aryan";
+//a = String(a);
+//console.log('age',age);
+//age = 23;
+//onsole.log("c+d", first+second);
+
+//console.log('age',age);
+//console.log('the value of a is',a);
+//string
+//a = 'name';
+//a = Boolean(a);
+
+const words =[" apple " ,"banana","cherry","12"]
+console.log(words);
